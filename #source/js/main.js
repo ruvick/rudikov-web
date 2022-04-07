fc4d48d3ee77a999a8baa34bf48eb1b135e9488c
Ruvick <rudikov.web@ya.ru>
2022-04-07 14:32:40 +0300

    Файлы сайта

---
+++ #source/js/main.js
@@ -0,0 +1,6 @@
+//@prepros-append jq-start.js
+//@prepros-append slick.min.js
+//@prepros-append jquery.inputmask.bundle.min.js
+//@prepros-append jquery.fancybox.min.js
+//@prepros-append script.js
+//@prepros-append jq-end.js      
